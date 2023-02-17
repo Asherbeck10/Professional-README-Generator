b1f0d2c48ebecb192c1fb038d791d6da82ffd53f
Asherbeck10 <asher.bec@gmail.com>
2023-02-17 07:08:35 +0000

    more changes to files

--- index.mjs
+++ index.mjs
@@ -1,21 +1,4 @@
-const fs = require("fs");
-const path = require('path');
-const inquirer = require("inquirer");
-const generateMarkdown = require("./utils/generateMarkdown");
 
-// array of questions for user
-const questions = [
+import inquirer from 'inquirer';
+import fs from "fs/promises";
 
-];
-
-// function to write README file
-function writeToFile(fileName, data) {
-}
-
-// function to initialize program
-function init() {
-
-}
-
-// function call to initialize program
-init();
